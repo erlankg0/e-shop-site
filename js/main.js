73e80f66fceb65f524011d9f541a6e8890c3dda9
Erlan Abdraimov <62596605+erlankg0@users.noreply.github.com>
2023-01-08 20:13:09 +0300

    banner

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(function(){
+
+    $('.banner-section__slider').slick({
+        dots: false,
+        prevArrow: '<button class="banner-section__slider-btn banner-section__slider-btnprev"><img src="images/arrow-left.svg" alt=""></button>',
+        nextArrow: '<button class="banner-section__slider-btn banner-section__slider-btnnext"><img src="images/arrow-right.svg" alt=""></button>',
+    });
+
+}); // End of use strict
